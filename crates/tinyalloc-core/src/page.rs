1d13e18c055b4a9f11bfa2e14a09739f592cab55
Fabrice <schaubfabrice@gmail.com>
2025-09-03 21:51:31 +0200

    advances

--- crates/tinyalloc-core/src/page.rs
+++ crates/tinyalloc-core/src/page.rs
@@ -2,42 +2,37 @@ use std::ptr::NonNull;
 
 use getset::Getters;
 
-use crate::vm::{MapError, Mapper};
+use crate::vm::{
+  MapError,
+  Mapper,
+};
 
 #[derive(Getters)]
 pub struct Page<'mapper> {
-    mapper: &'mapper dyn Mapper,
-    #[getset(get = "pub")]
-    ptr: NonNull<[u8]>,
+  mapper: &'mapper dyn Mapper,
+  #[getset(get = "pub")]
+  ptr: NonNull<[u8]>,
 }
 
 impl<'mapper> Page<'mapper> {
-    pub fn new(mapper: &'mapper dyn Mapper, size: usize) -> Result<Self, MapError> {
-        let ptr = mapper.map(size)?;
-        Ok(Self { mapper, ptr })
-    }
+  pub fn new(mapper: &'mapper dyn Mapper, size: usize) -> Result<Self, MapError> {
+    let ptr = mapper.map(size)?;
+    Ok(Self { mapper, ptr })
+  }
 
-    pub fn as_ptr(&mut self) -> NonNull<u8> {
-        unsafe { NonNull::new_unchecked(self.ptr.as_mut().as_mut_ptr()) }
-    }
+  pub fn commit(&mut self) -> Result<(), MapError> {
+    self.mapper.commit(self.ptr)
+  }
 
-    pub fn len(&self) -> usize {
-        self.ptr.len()
-    }
+  pub fn decommit(&mut self) -> Result<(), MapError> {
+    self.mapper.decommit(self.ptr)
+  }
 
-    pub fn commit(&mut self) -> Result<(), MapError> {
-        self.mapper.commit(self.as_ptr(), self.len())
-    }
+  pub fn protect(&mut self) -> Result<(), MapError> {
+    self.mapper.protect(self.ptr)
+  }
 
-    pub fn decommit(&mut self) -> Result<(), MapError> {
-        self.mapper.decommit(self.as_ptr(), self.len())
-    }
-
-    pub fn protect(&mut self) -> Result<(), MapError> {
-        self.mapper.protect(self.as_ptr(), self.len())
-    }
-
-    pub fn unprotect(&mut self) -> Result<(), MapError> {
-        self.mapper.unprotect(self.as_ptr(), self.len())
-    }
+  pub fn unprotect(&mut self) -> Result<(), MapError> {
+    self.mapper.unprotect(self.ptr)
+  }
 }
--- crates/tinyalloc-core/src/vm.rs
+++ crates/tinyalloc-core/src/vm.rs
@@ -3,10 +3,27 @@ use std::ptr::NonNull;
 pub struct MapError;
 
 pub trait Mapper {
-    fn map(&self, size: usize) -> Result<NonNull<[u8]>, MapError>;
-    fn unmap(&self, ptr: NonNull<u8>, size: usize);
-    fn commit(&self, ptr: NonNull<u8>, size: usize) -> Result<(), MapError>;
-    fn decommit(&self, ptr: NonNull<u8>, size: usize) -> Result<(), MapError>;
-    fn protect(&self, ptr: NonNull<u8>, size: usize) -> Result<(), MapError>;
-    fn unprotect(&self, ptr: NonNull<u8>, size: usize) -> Result<(), MapError>;
+    fn map(&self, size: usize) -> Result<NonNull<[u8]>, MapError> {
+        _ = size;
+        return Err(MapError);
+    }
+    fn unmap(&self, ptr: NonNull<[u8]>) {
+        _ = ptr;
+    }
+    fn commit(&self, ptr: NonNull<[u8]>) -> Result<(), MapError> {
+        _ = ptr;
+        return Err(MapError);
+    }
+    fn decommit(&self, ptr: NonNull<[u8]>) -> Result<(), MapError> {
+        _ = ptr;
+        return Err(MapError);
+    }
+    fn protect(&self, ptr: NonNull<[u8]>) -> Result<(), MapError> {
+        _ = ptr;
+        return Err(MapError);
+    }
+    fn unprotect(&self, ptr: NonNull<[u8]>) -> Result<(), MapError> {
+        _ = ptr;
+        return Err(MapError);
+    }
 }
