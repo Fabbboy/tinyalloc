e0839c0c6a12be995f74a5cf581ca02e09a71219
Fabrice <fabrice.schaub@swisscom.com>
2025-09-04 13:22:10 +0200

    making queue clear

--- crates/tinyalloc-container/src/queue.rs
+++ crates/tinyalloc-container/src/queue.rs
@@ -5,6 +5,8 @@ use getset::{
   MutGetters,
 };
 
+// Philosophy: A non-owning singly linked list
+
 #[derive(Getters, MutGetters)]
 pub struct Node<T> {
   #[getset(get = "pub", get_mut = "pub")]
@@ -31,6 +33,34 @@ impl<T> Queue<T> {
       head: None,
       current: None,
       len: 0,
+    }
+  }
+
+  pub fn push(&mut self, node: NonNull<Node<T>>) {
+    unsafe {
+      if let Some(mut current) = self.current {
+        current.as_mut().next = Some(node);
+        self.current = Some(node);
+      } else {
+        self.head = Some(node);
+        self.current = Some(node);
+      }
+      self.len += 1;
+    }
+  }
+
+  pub fn pop(&mut self) -> Option<NonNull<Node<T>>> {
+    if let Some(head) = self.head {
+      unsafe {
+        self.head = head.as_ref().next;
+        if self.head.is_none() {
+          self.current = None;
+        }
+      }
+      self.len -= 1;
+      Some(head)
+    } else {
+      None
     }
   }
 }
