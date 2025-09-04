a50cbdc7e7bd53bbdd2516b59e58fda1cfb7050a
Fabrice <fabrice.schaub@swisscom.com>
2025-09-04 13:34:10 +0200

    containers

--- crates/tinyalloc-container/src/mvec.rs
+++ crates/tinyalloc-container/src/mvec.rs
@@ -1,27 +1,185 @@
-use std::ptr::NonNull;
+use std::{
+  mem,
+  ptr::{
+    self,
+    NonNull,
+  },
+};
 
 use tinyalloc_sys::{
   page::Page,
-  vm::Mapper,
+  size::page_size,
+  vm::{
+    MapError,
+    Mapper,
+  },
 };
 
 use crate::queue::Queue;
 
-// Philosophy: syscalls are DAMN slow so DON'T ALLOCATE ANYTHING UNTIL WE ACTUALLY APPEND TO THE VECTOR
-// Resizes are bigger in order to prevent often resizes
-// Instead of unmapping decommit unused pages after a threshold
-// if another threshold is reached, we unmap the whole thing
+pub const MVEC_GROWTH_FACTOR: usize = 2;
+pub const MVEC_DECOMMIT_THRESHOLD: f32 = 0.25;
+pub const MVEC_UNMAP_THRESHOLD: f32 = 0.125;
 
 pub struct MappedVector<'mapper, T> {
   data: Option<NonNull<T>>,
+  len: usize,
+  capacity: usize,
   backing: Queue<'mapper, Page<'mapper>>,
+  mapper: &'mapper dyn Mapper,
 }
 
 impl<'mapper, T> MappedVector<'mapper, T> {
   pub fn new(mapper: &'mapper dyn Mapper) -> Self {
     Self {
       data: None,
+      len: 0,
+      capacity: 0,
       backing: Queue::new(mapper),
+      mapper,
     }
+  }
+
+  fn elements_per_page(&self) -> usize {
+    page_size() / mem::size_of::<T>()
+  }
+
+  fn ensure_capacity(&mut self, min_capacity: usize) -> Result<(), MapError> {
+    if self.capacity >= min_capacity {
+      return Ok(());
+    }
+
+    let new_capacity = if self.capacity == 0 {
+      min_capacity.max(self.elements_per_page())
+    } else {
+      (self.capacity * MVEC_GROWTH_FACTOR).max(min_capacity)
+    };
+
+    let pages_needed =
+      (new_capacity * mem::size_of::<T>() + page_size() - 1) / page_size();
+
+    for _ in 0..pages_needed {
+      let page = Page::new(self.mapper, page_size())?;
+      self.backing.push(page)?;
+    }
+
+    if self.data.is_none() {
+      let first_page = Page::new(self.mapper, page_size())?;
+      let data_ptr = first_page.as_ref().as_ptr() as *mut T;
+      self.data = Some(unsafe { NonNull::new_unchecked(data_ptr) });
+      self.backing.push(first_page)?;
+    }
+
+    self.capacity = new_capacity;
+    Ok(())
+  }
+
+  pub fn push(&mut self, value: T) -> Result<(), MapError> {
+    self.ensure_capacity(self.len + 1)?;
+
+    unsafe {
+      let data_ptr = self.data.unwrap().as_ptr();
+      ptr::write(data_ptr.add(self.len), value);
+      self.len += 1;
+    }
+
+    Ok(())
+  }
+
+  pub fn pop(&mut self) -> Option<T> {
+    if self.len == 0 {
+      return None;
+    }
+
+    unsafe {
+      self.len -= 1;
+      let data_ptr = self.data.unwrap().as_ptr();
+      Some(std::ptr::read(data_ptr.add(self.len)))
+    }
+  }
+
+  pub fn get(&self, index: usize) -> Option<&T> {
+    if index >= self.len {
+      return None;
+    }
+
+    unsafe {
+      let data_ptr = self.data?.as_ptr();
+      Some(&*data_ptr.add(index))
+    }
+  }
+
+  pub fn get_mut(&mut self, index: usize) -> Option<&mut T> {
+    if index >= self.len {
+      return None;
+    }
+
+    unsafe {
+      let data_ptr = self.data?.as_ptr();
+      Some(&mut *data_ptr.add(index))
+    }
+  }
+
+  pub fn len(&self) -> usize {
+    self.len
+  }
+
+  pub fn capacity(&self) -> usize {
+    self.capacity
+  }
+
+  pub fn is_empty(&self) -> bool {
+    self.len == 0
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use tinyalloc_sys::vm::Mapper;
+
+  #[cfg(unix)]
+  use tinyalloc_sys::system::posix::PosixMapper;
+  #[cfg(windows)]
+  use tinyalloc_sys::system::windows::WindowsMapper;
+
+  #[cfg(unix)]
+  static BACKING_MAPPER: PosixMapper = PosixMapper;
+  #[cfg(windows)]
+  static BACKING_MAPPER: WindowsMapper = WindowsMapper;
+
+  static MAPPER: &dyn Mapper = &BACKING_MAPPER;
+
+  #[test]
+  fn test_new() {
+    let mvec: MappedVector<i32> = MappedVector::new(MAPPER);
+    assert_eq!(mvec.len(), 0);
+    assert_eq!(mvec.capacity(), 0);
+    assert!(mvec.is_empty());
+  }
+
+  #[test]
+  fn test_push_pop() {
+    let mut mvec = MappedVector::new(MAPPER);
+
+    mvec.push(1).unwrap();
+    mvec.push(2).unwrap();
+
+    assert_eq!(mvec.len(), 2);
+    assert_eq!(mvec.pop(), Some(2));
+    assert_eq!(mvec.pop(), Some(1));
+    assert_eq!(mvec.pop(), None);
+  }
+
+  #[test]
+  fn test_get() {
+    let mut mvec = MappedVector::new(MAPPER);
+
+    mvec.push(10).unwrap();
+    mvec.push(20).unwrap();
+
+    assert_eq!(mvec.get(0), Some(&10));
+    assert_eq!(mvec.get(1), Some(&20));
+    assert_eq!(mvec.get(2), None);
   }
 }
--- crates/tinyalloc-container/src/queue.rs
+++ crates/tinyalloc-container/src/queue.rs
@@ -6,8 +6,14 @@ use getset::{
 };
 use tinyalloc_sys::{
   page::Page,
-  vm::Mapper,
+  size::page_size,
+  vm::{
+    MapError,
+    Mapper,
+  },
 };
+
+pub const QUEUE_NODE_ALIGNMENT: usize = 8;
 
 #[derive(Getters, MutGetters)]
 pub struct Node<T> {
@@ -25,9 +31,10 @@ impl<T> Node<T> {
 
 pub struct Queue<'mapper, T> {
   head: Option<NonNull<Node<T>>>,
-  current: Option<NonNull<Node<T>>>,
+  tail: Option<NonNull<Node<T>>>,
   len: usize,
-  data: Option<Page<'mapper>>, // everything is allocated inside here and the arena is **REPLACED** when more space is needed because most OS do not support remapping. Additionally, this is an Option **ONLY** allocated when something is actually pushed
+  data: Option<Page<'mapper>>,
+  allocated_nodes: usize,
   system: &'mapper dyn Mapper,
 }
 
@@ -35,15 +42,136 @@ impl<'mapper, T> Queue<'mapper, T> {
   pub fn new(system: &'mapper dyn Mapper) -> Self {
     Self {
       head: None,
-      current: None,
+      tail: None,
       len: 0,
       data: None,
+      allocated_nodes: 0,
       system,
     }
   }
 
-  pub fn push(&mut self, value: T) {}
+  fn nodes_per_page(&self) -> usize {
+    page_size() / std::mem::size_of::<Node<T>>()
+  }
+
+  fn ensure_capacity(&mut self) -> Result<(), MapError> {
+    if self.data.is_none() || self.allocated_nodes >= self.nodes_per_page() {
+      let new_page = Page::new(self.system, page_size())?;
+      self.data = Some(new_page);
+      self.allocated_nodes = 0;
+    }
+    Ok(())
+  }
+
+  fn allocate_node(&mut self, value: T) -> Result<NonNull<Node<T>>, MapError> {
+    self.ensure_capacity()?;
+
+    let page = self.data.as_mut().unwrap();
+    let page_slice = page.as_mut();
+
+    unsafe {
+      let base_ptr = page_slice.as_mut_ptr() as *mut Node<T>;
+      let node_ptr = base_ptr.add(self.allocated_nodes);
+
+      std::ptr::write(node_ptr, Node::new(value));
+      self.allocated_nodes += 1;
+
+      Ok(NonNull::new_unchecked(node_ptr))
+    }
+  }
+
+  pub fn push(&mut self, value: T) -> Result<(), MapError> {
+    let new_node = self.allocate_node(value)?;
+
+    unsafe {
+      if let Some(tail) = self.tail {
+        (*tail.as_ptr()).next = Some(new_node);
+      } else {
+        self.head = Some(new_node);
+      }
+
+      self.tail = Some(new_node);
+      self.len += 1;
+    }
+
+    Ok(())
+  }
+
   pub fn pop(&mut self) -> Option<T> {
-    None
+    let head = self.head?;
+
+    unsafe {
+      let head_ref = head.as_ref();
+      let value = std::ptr::read(&head_ref.value);
+
+      self.head = head_ref.next;
+
+      if self.head.is_none() {
+        self.tail = None;
+      }
+
+      self.len = self.len.saturating_sub(1);
+
+      Some(value)
+    }
+  }
+
+  pub fn len(&self) -> usize {
+    self.len
+  }
+
+  pub fn is_empty(&self) -> bool {
+    self.len == 0
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use tinyalloc_sys::vm::Mapper;
+
+  #[cfg(unix)]
+  use tinyalloc_sys::system::posix::PosixMapper;
+  #[cfg(windows)]
+  use tinyalloc_sys::system::windows::WindowsMapper;
+
+  #[cfg(unix)]
+  static BACKING_MAPPER: PosixMapper = PosixMapper;
+  #[cfg(windows)]
+  static BACKING_MAPPER: WindowsMapper = WindowsMapper;
+
+  static MAPPER: &dyn Mapper = &BACKING_MAPPER;
+
+  #[test]
+  fn test_new() {
+    let queue: Queue<i32> = Queue::new(MAPPER);
+    assert_eq!(queue.len(), 0);
+    assert!(queue.is_empty());
+  }
+
+  #[test]
+  fn test_push_pop() {
+    let mut queue = Queue::new(MAPPER);
+
+    queue.push(1).unwrap();
+    queue.push(2).unwrap();
+
+    assert_eq!(queue.len(), 2);
+    assert_eq!(queue.pop(), Some(1));
+    assert_eq!(queue.pop(), Some(2));
+    assert_eq!(queue.pop(), None);
+  }
+
+  #[test]
+  fn test_fifo_order() {
+    let mut queue = Queue::new(MAPPER);
+
+    for i in 0..10 {
+      queue.push(i).unwrap();
+    }
+
+    for i in 0..10 {
+      assert_eq!(queue.pop(), Some(i));
+    }
   }
 }
