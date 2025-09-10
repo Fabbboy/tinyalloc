48ec8f12f13ffbfd94a3dbd6d0d9d7c3552a49a9
Fabrice <schaubfabrice@gmail.com>
2025-09-10 23:19:03 +0200

    working on linkjed list

--- crates/tinyalloc-list/src/lib.rs
+++ crates/tinyalloc-list/src/lib.rs
@@ -2,12 +2,33 @@ use std::ptr::NonNull;
 
 use thiserror::Error;
 
-pub trait Item {
-    fn next(&self) -> Option<NonNull<Self>>;
-    fn prev(&self) -> Option<NonNull<Self>>;
-    fn set_next(&mut self, next: Option<NonNull<Self>>);
-    fn set_prev(&mut self, prev: Option<NonNull<Self>>);
+pub trait Item<T> {
+    fn next(&self) -> Option<NonNull<T>>;
+    fn prev(&self) -> Option<NonNull<T>>;
+    fn set_next(&mut self, next: Option<NonNull<T>>);
+    fn set_prev(&mut self, prev: Option<NonNull<T>>);
 }
 
 #[derive(Debug, Error)]
 pub enum ListError {}
+
+#[derive(Debug)]
+pub struct List<T>
+where
+    T: Item<T>,
+{
+    head: Option<NonNull<T>>,
+    tail: Option<NonNull<T>>,
+}
+
+impl<T> List<T>
+where
+    T: Item<T>,
+{
+    pub const fn new() -> Self {
+        Self {
+            head: None,
+            tail: None,
+        }
+    }
+}
