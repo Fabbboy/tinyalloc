f9d204feee22b91f7ec45c0e0dfe2446b1309590
Fabrice <schaubfabrice@gmail.com>
2025-09-14 20:12:47 +0200

    added scaffolding

--- crates/tinyalloc-alloc/src/heap.rs
+++ crates/tinyalloc-alloc/src/heap.rs
@@ -1,3 +1,5 @@
+use std::{alloc::{GlobalAlloc, Layout}, ptr::NonNull};
+
 use crate::{classes::class_init, config::SIZES, queue::Queue};
 
 pub struct Heap<'mapper> {
@@ -8,5 +10,22 @@ impl<'mapper> Heap<'mapper> {
     pub fn new() -> Self {
         let classes: [Queue<'mapper>; SIZES] = class_init(|class| Queue::new(class));
         Self { classes }
+    }
+
+    pub fn allocate(&mut self, layout: Layout) -> Option<NonNull<[u8]>> {
+        todo!()
+    }
+    pub fn deallocate(&mut self, ptr: NonNull<u8>, layout: Layout) {
+      todo!()
+    }
+}
+
+unsafe impl<'mapper> GlobalAlloc for Heap<'mapper> {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        todo!()
+    }
+
+    unsafe fn dealloc(&self, ptr: *mut u8, layout: Layout) {
+        todo!()
     }
 }
