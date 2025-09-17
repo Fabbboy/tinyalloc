90695f307219c0a8e6c0b40df9010a6af66e7462
Fabrice <schaubfabrice@gmail.com>
2025-09-17 22:54:42 +0200

    guarding teardown

--- src/init.rs
+++ src/init.rs
@@ -1,14 +1,31 @@
-use std::sync::atomic::AtomicBool;
+use std::sync::atomic::{
+  AtomicBool,
+  Ordering,
+};
 
-static TEARDOWN: AtomicBool = AtomicBool::new(false);
-struct TeardownGuard {}
+struct TeardownGuard;
 
 impl Drop for TeardownGuard {
   fn drop(&mut self) {
-
+    TEARING_DOWN.with(|flag| {
+      flag.store(true, Ordering::SeqCst);
+    });
   }
+}
+
+#[inline]
+pub fn td_register() {
+  _GUARD.with(|_| {});
+}
+
+#[inline]
+pub fn is_td() -> bool {
+  TEARING_DOWN
+    .try_with(|flag| flag.load(Ordering::SeqCst))
+    .unwrap_or(true)
 }
 
 thread_local! {
   static _GUARD: TeardownGuard = TeardownGuard {};
+  pub static TEARING_DOWN: AtomicBool = AtomicBool::new(false);
 }
